34a206fa7810e4bcb5d05f9eb53a59163bba9360
Kush <kp673@njit.edu>
2021-03-04 01:17:55 -0500

    connect MainActivity to backend

--- app/src/main/java/kp673/parstagram/MainActivity.java
+++ app/src/main/java/kp673/parstagram/MainActivity.java
@@ -3,9 +3,16 @@ package kp673.parstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +31,24 @@ public class MainActivity extends AppCompatActivity {
         btnCaptureImage=findViewById(R.id.btnCaptureImage);
         ivPreview= findViewById(R.id.ivPreview);
         btnPost=findViewById(R.id.btnPost);
+        queryPost();
 
+    }
+
+    private void queryPost() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e!= null){
+                    Log.e(TAG,"Couldn't find content",e);
+                    return;
+                }
+                for (Post post: posts){
+                    Log.i(TAG, "Post: "+post.getDescription()+ " username: "+post.getUser().getUsername());
+                }
+            }
+        });
     }
 }
