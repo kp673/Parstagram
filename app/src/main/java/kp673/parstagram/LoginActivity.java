a123be6239538ca0c6c86d78deec167aa2717f4f
Kush <kp673@njit.edu>
2021-03-07 01:42:48 -0500

    bug fixes

--- app/src/main/java/kp673/parstagram/LoginActivity.java
+++ app/src/main/java/kp673/parstagram/LoginActivity.java
@@ -30,9 +30,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        if (ParseUser.getCurrentUser() != null){
-//            gotoMainActivity();
-//        }
+        if (ParseUser.getCurrentUser() != null){
+            gotoMainActivity();
+        }
 
         etUsername=findViewById(R.id.etUsername);
         etPassword=findViewById(R.id.etPassword);
